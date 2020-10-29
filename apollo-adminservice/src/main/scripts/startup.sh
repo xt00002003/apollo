9a52019671469c2beb3f4e757d56d2aaf56f2d28
薛腾 <xt00002003@163.com>
2020-10-29 16:41:31 +0800

    修改服务的端口地址

--- apollo-adminservice/src/main/scripts/startup.sh
+++ apollo-adminservice/src/main/scripts/startup.sh
@@ -3,7 +3,7 @@ SERVICE_NAME=apollo-adminservice
 ## Adjust log dir if necessary
 LOG_DIR=/opt/logs/100003172
 ## Adjust server port if necessary
-SERVER_PORT=${SERVER_PORT:=8090}
+SERVER_PORT=${SERVER_PORT:=7183}
 
 ## Create log directory if not existed because JDK 8+ won't do that
 mkdir -p $LOG_DIR
--- apollo-portal/src/main/scripts/startup.sh
+++ apollo-portal/src/main/scripts/startup.sh
@@ -3,7 +3,7 @@ SERVICE_NAME=apollo-portal
 ## Adjust log dir if necessary
 LOG_DIR=/opt/logs/100003173
 ## Adjust server port if necessary
-SERVER_PORT=${SERVER_PORT:=8070}
+SERVER_PORT=${SERVER_PORT:=7182}
 
 ## Create log directory if not existed because JDK 8+ won't do that
 mkdir -p $LOG_DIR
